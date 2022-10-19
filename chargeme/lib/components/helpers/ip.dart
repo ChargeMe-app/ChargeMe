56e21a38968232f76d9fab086a6157834f0f20fe
Nikita Ratashnyuk <nikitarat1@gmail.com>
2022-10-19 08:33:29 +0300

    new build

--- chargeme/lib/components/helpers/ip.dart
+++ chargeme/lib/components/helpers/ip.dart
@@ -1,7 +1,7 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
 class IP {
-  static String current = "158.160.13.205";
+  static String current = "158.160.15.192";
   static String port = "8081";
 
   static void changeToDebugIPIfNeeded() async {
